07265c61e85de900514f18225b39abdc5b267168
Daniel <dbullock377@comcast.net>
2020-10-18 16:43:41 -0500

    baseline done

--- baseline.py
+++ baseline.py
@@ -2,6 +2,7 @@
 Part 1: Simple baseline that only uses word statistics to predict tags
 """
 
+
 def baseline(train, test):
     '''
     input:  training data (list of sentences, with tags on the words)
@@ -9,5 +10,25 @@ def baseline(train, test):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    
-    return []
+
+    output = []
+    word_map = {}
+
+    for sentence in train:
+        for word, tag in sentence:
+            if word not in word_map:
+                word_map[word] = {}
+            if tag not in word_map[word]:
+                word_map[word][tag] = 1
+            else:
+                word_map[word][tag] += 1
+
+    for sentence in test:
+        temp = []
+        for word in sentence:
+            if word in word_map:
+                temp.append((word, max(word_map[word], key=lambda x: word_map[word][x])))
+            else:
+                temp.append((word, 'NOUN'))
+        output.append(temp)
+    return output
